8827ffe2d78beccc65b578f7bf34e71731777f6d
sylhare <sylhare@outlook.com>
2021-02-01 11:10:33 -0500

    Update todo container

--- src/create-app/src/App.js
+++ src/create-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './resources/logo.svg';
 import './App.css';
+import TodoContainer from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +19,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <TodoContainer todos={[
+        {
+          title: 'todo1'
+        },
+        {
+          title: 'todo2'
+        }]}/>
     </div>
   );
 }
