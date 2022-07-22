f81f298fc130cafa529118410c8c39163929a3e7
sylhare <sylhare@outlook.com>
2022-07-22 09:14:56 -0400

    Update store

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -1,12 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, PreloadedState } from '@reduxjs/toolkit';
 import { counterReducer } from './counter/slice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+
+
+// Create the root reducer separately so we can extract the RootState type
+const rootReducer = combineReducers({
+  counter: counterReducer,
+})
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState
+  })
+}
+
+export const store = setupStore()
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>
 export default store;
