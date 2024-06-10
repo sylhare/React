e66851e558c91bd667ee88dc033079f31a3082ee
sylhare <sylhare@outlook.com>
2024-06-10 13:22:18 -0400

    Set up Chakra App

--- examples/chakra-ui/src/index.tsx
+++ examples/chakra-ui/src/index.tsx
@@ -1,13 +1,54 @@
-import * as React from 'react'
-
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Card, CardBody, CardHeader, ChakraProvider, Container, Text } from '@chakra-ui/react';
 
-function App() {
+function App(): React.JSX.Element {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <TheRestOfYourApplication />
+      {/* the rest of your app */}
+
+      <Container
+        paddingTop={5}
+        minHeight={'100vh'}
+        bg={'lightgray'}
+        {...{
+          maxW: {
+            base: 'container.xl',
+            '4xl': 'container.4xl',
+            '5xl': 'container.5xl',
+            '6xl': 'container.6xl',
+          },
+        }}>
+        <Card p={5}>
+          <CardHeader
+            borderBottom={'1px solid'}
+            borderColor={'#f5f5f5'}
+          >
+            Chakra UI example
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Welcome to Chakra UI
+            </Text>
+          </CardBody>
+        </Card>
+      </Container>
     </ChakraProvider>
-  )
+  );
+}
+
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App/>
+      </ChakraProvider>
+    </React.StrictMode>,
+  );
+} else {
+  console.log('Root element not found');
 }
