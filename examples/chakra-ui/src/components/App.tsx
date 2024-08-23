b5fcb17abb6e6bda059135a668960243c94368b3
sylhare <sylhare@outlook.com>
2024-08-23 11:50:36 -0400

    Re-arrange project

---
+++ examples/chakra-ui/src/components/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card, CardBody, CardHeader, ChakraProvider, Container, Text } from '@chakra-ui/react';
+import { ExampleAligned } from './ExampleAligned';
+import { ExampleForm } from './forms/ExampleForm';
+
+export function App(): React.JSX.Element {
+  return (
+    <ChakraProvider>
+      <Container
+        paddingTop={5}
+        minHeight={'100vh'}
+        bg={'lightgray'}
+        {...{
+          maxW: {
+            base: 'container.xl',
+            '4xl': 'container.4xl',
+            '5xl': 'container.5xl',
+            '6xl': 'container.6xl',
+          },
+        }}>
+        <Card p={5}>
+          <CardHeader
+            borderBottom={'1px solid'}
+            borderColor={'#f5f5f5'}
+          >
+            Chakra UI example
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Welcome to Chakra UI
+            </Text>
+            <ExampleAligned/>
+            <ExampleForm/>
+          </CardBody>
+        </Card>
+      </Container>
+    </ChakraProvider>
+  );
+}
--- examples/chakra-ui/src/index.tsx
+++ examples/chakra-ui/src/index.tsx
@@ -1,43 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Card, CardBody, CardHeader, ChakraProvider, Container, Text } from '@chakra-ui/react';
-import { ExampleAligned } from './components/ExampleAligned';
-import { ExampleForm } from './components/ExampleForm';
+import { ChakraProvider } from '@chakra-ui/react';
+import { App } from './components/App';
 
-function App(): React.JSX.Element {
-  return (
-    <ChakraProvider>
-      <Container
-        paddingTop={5}
-        minHeight={'100vh'}
-        bg={'lightgray'}
-        {...{
-          maxW: {
-            base: 'container.xl',
-            '4xl': 'container.4xl',
-            '5xl': 'container.5xl',
-            '6xl': 'container.6xl',
-          },
-        }}>
-        <Card p={5}>
-          <CardHeader
-            borderBottom={'1px solid'}
-            borderColor={'#f5f5f5'}
-          >
-            Chakra UI example
-          </CardHeader>
-          <CardBody>
-            <Text>
-              Welcome to Chakra UI
-            </Text>
-            <ExampleAligned/>
-            <ExampleForm/>
-          </CardBody>
-        </Card>
-      </Container>
-    </ChakraProvider>
-  );
-}
 
 const root = document.getElementById('root');
 
