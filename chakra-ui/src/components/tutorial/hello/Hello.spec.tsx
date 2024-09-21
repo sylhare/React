172f4ac8bc8e104c9591850b9fba5cece2df0328
sylhare <sylhare@outlook.com>
2024-09-21 08:11:20 -0400

    Update Mock tests

--- chakra-ui/src/components/tutorial/hello/Hello.spec.tsx
+++ chakra-ui/src/components/tutorial/hello/Hello.spec.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { Hello } from './Hello';
 
 describe('Hello', () => {
-
   it('is a dummy test', () => {
     expect(true).toBeTruthy();
   });
 
   it('renders', () => {
-    const { getByText } = render(<Hello name="World"/>);
+    const { getByText } = render(<Hello name={'World'}/>);
     expect(getByText('Hello World')).toBeVisible();
   });
 
+  it('renders async', async () => {
+    const { getByText } = render(<Hello name={'Async'}/>);
+    await waitFor(() => expect(getByText('Hello Async')).toBeVisible());
+  });
 });
