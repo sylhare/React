4e9b53b06f20604877cec0620761477c16a4f8da
sylhare <sylhare@outlook.com>
2024-10-02 10:45:55 -0400

    Update Mess

--- chakra-ui/src/components/tutorial/Mess.tsx
+++ chakra-ui/src/components/tutorial/Mess.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //import useBoolean from './../../hooks/useBoolean';
 import { Box, Button, Text } from '@chakra-ui/react';
 import useBoolean from '@_hooks/useBoolean';
@@ -6,12 +6,22 @@ import useBoolean from '@_hooks/useBoolean';
 
 export const Mess = () => {
   const { value: isToggled, setTrue, setFalse } = useBoolean();
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('value changed', value);
+  }, [value]);
+
+  const turnOn = () => {
+    setTrue();
+    setValue(value + 1);
+  };
 
   return (
     <Box>
-      <Text pb={2}>The toggle is <Box as='span' fontWeight={'semibold'}>{isToggled ? 'ON 💡' : 'OFF 💤'}</Box></Text>
-      <Button m={1} variant={'solid'} onClick={setTrue}>Turn ON</Button>
+      <Text pb={2}>The toggle is <Box as="span" fontWeight={'semibold'}>{isToggled ? 'ON 💡' : 'OFF 💤'}</Box></Text>
+      <Button m={1} variant={'solid'} onClick={turnOn}>Turn ON</Button>
       <Button m={1} variant={'outline'} onClick={setFalse}>Turn Off</Button>
     </Box>
   );
-}
+};
