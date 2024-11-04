6c71a371913a87ebd02f2f8f16f53d604bcd6fc7
sylhare <sylhare@outlook.com>
2024-11-04 10:22:40 -0500

    Update Hello

--- chakra-ui/src/components/tutorial/hello/Hello.tsx
+++ chakra-ui/src/components/tutorial/hello/Hello.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HelloProps } from './types';
 
-export const Hello: React.FC<HelloProps> = ({ name }) => {
+export const Hello: React.FC<HelloProps> = ({ name }): React.JSX.Element => {
   return <div>Hello {name}</div>;
 }
--- chakra-ui/src/components/tutorial/hello/types.ts
+++ chakra-ui/src/components/tutorial/hello/types.ts
@@ -1,3 +1,9 @@
+import { Strategy } from '@_components/tutorial/hello/Example';
+
 export interface HelloProps {
-  name: string
+  name: string;
+}
+
+export interface GenericProps {
+  strategy: Strategy;
 }
