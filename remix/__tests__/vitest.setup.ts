8b3661cc0155eafcb9fa71322ed3574164a26040
sylhare <sylhare@outlook.com>
2024-11-15 09:23:43 -0500

    Update config

--- remix/__tests__/vitest.setup.ts
+++ remix/__tests__/vitest.setup.ts
@@ -1,1 +1,6 @@
-import '@testing-library/jest-dom/vitest'
+import { installGlobals } from '@remix-run/node';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom/matchers';
+
+installGlobals();
