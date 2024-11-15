c9e2165dc7c6ec11b8a1f2a3cef6c76de43d8bd8
sylhare <sylhare@outlook.com>
2024-11-15 09:23:43 -0500

    Update config

--- remix/vitest.config.ts
+++ remix/vitest.config.ts
@@ -1,11 +1,26 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./__tests__/vitest.setup.ts'],
+    watch: false,
+    globals: true,
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: './coverage',
+      reporter: ['lcov', 'text'],
+      provider: 'v8',
+      exclude: [
+        '**/coverage/**',
+        '**/*.config.js',
+        '**/*.config.ts',
+        '**/build/**',
+      ],
+    },
   },
 });
