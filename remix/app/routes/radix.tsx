791cee94c188c72bd5b6181d81acaf3e628bcdc4
sylhare <sylhare@outlook.com>
2025-01-13 11:43:41 -0500

    Add select demo

--- remix/app/routes/radix.tsx
+++ remix/app/routes/radix.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Button, Checkbox, Container, Flex, Switch, Text, Theme } from '@radix-ui/themes';
+import { Box, Container, Flex, Theme } from '@radix-ui/themes';
 import { CompanyCard } from '~/radix/CompanyCard';
 import { FileUpload } from '~/radix/FileUpload';
+import { FormDemo } from '~/radix/FormDemo';
+import { SelectDemo } from '~/radix/SelectDemo';
+import { PrimitivesDemo } from '~/radix/PrimitivesDemo';
+import { ContextMenuDemo } from '~/radix/ContextMenuDemo';
 
 export default function RadixPage(): React.JSX.Element {
   return (
@@ -9,9 +13,13 @@ export default function RadixPage(): React.JSX.Element {
       <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
         <Container style={{ backgroundColor: 'var(--gray-1)', marginTop: '20px' }}>
           <Flex direction="column" align="center" gap="9">
-            <CompanyCard/>
-            <FileUpload/>
             <PrimitivesDemo />
+            <ContextMenuDemo />
+            <SelectDemo />
+            <CompanyCard />
+            <FileUpload />
+            <FormDemo />
+            <Box mt={'9'} />
           </Flex>
         </Container>
       </Theme>
