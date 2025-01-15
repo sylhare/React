b20970e3142e9ac61e83c3943cb2b08d397c41cc
sylhare <sylhare@outlook.com>
2025-01-15 09:03:15 -0500

    Update file upload

--- remix/app/radix/FileUpload.tsx
+++ remix/app/radix/FileUpload.tsx
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Button, Flex, Text } from '@radix-ui/themes';
+import './FileUpload.css';
+import { DownloadIcon } from '@radix-ui/react-icons';
 
 export function FileUpload() {
   const [file, setFile] = useState<File | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setFile(event.target.files[0]);
     }
+  };
+
+  const handleDrop = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+    if (event.dataTransfer.files) {
+      setFile(event.dataTransfer.files[0]);
+    }
+  }, []);
+
+  const handleDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  }, []);
+
+  const handleDragLeave = useCallback(() => {
+    setIsDragging(false);
+  }, []);
+
+  const handleRemoveFile = () => {
+    setFile(null);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -16,7 +41,6 @@ export function FileUpload() {
       const formData = new FormData();
       formData.append('file', file);
 
-      // Replace with your file upload URL
       fetch('/upload', {
         method: 'POST',
         body: formData,
@@ -31,11 +55,39 @@ export function FileUpload() {
     }
   };
 
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <Flex direction="column" align="center" gap="3">
-        <Text> Select a file to upload:</Text>
-        <input type="file" onChange={handleFileChange} data-testid="file-input"/>
+        <Text>Select a file to upload:</Text>
+        <Flex
+          className={`drop-zone ${isDragging ? 'dragging' : ''}`}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onClick={handleClick}
+          direction={'column'}
+          gap={'4'}
+        >
+          <input
+            type="file"
+            onChange={handleFileChange}
+            data-testid="file-input"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+          />
+          <DownloadIcon />
+          <Text>{file ? `${file.name}` : 'Drag & drop a file here, or click to select one'}</Text>
+
+          {file && (
+            <Button variant="ghost" size="2" onClick={handleRemoveFile}>
+                Remove
+            </Button>
+          )}
+        </Flex>
         <Button type="submit">Upload</Button>
       </Flex>
     </form>
