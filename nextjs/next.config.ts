e2c9aff2d8ce7b3b5ae0ee2fe46588da18eb208c
sylhare <sylhare@outlook.com>
2026-02-20 16:50:51 -0500

    Fix package setup

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@transifex/native', '@transifex/react'],
 };
 
 export default nextConfig;
--- nextjs/playwright.config.ts
+++ nextjs/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 2,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
